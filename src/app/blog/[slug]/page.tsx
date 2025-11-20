6e46364513ad41a5c23d73562aeb46e6e8ba0dab
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-11-20 07:26:05 -0300

    feat: implement dynamic SEO metadata generation for blog posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,14 +1,44 @@
-import { PostPage } from "@/templates/blog";
-import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { allPosts } from "contentlayer/generated";
+import { PostPage } from "@/templates/blog";
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
+
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
 
